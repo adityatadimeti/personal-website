8ca9dd81341d3611123bd0a639e7d6ad2b19006b
Aditya <adityatadimeti@gmail.com>
2022-07-14 22:45:15 -0700

    142 review

--- src/routes/Education/CourseReviews.js
+++ src/routes/Education/CourseReviews.js
@@ -26,9 +26,6 @@ function CourseReviews() {
                       CS 142: Web Applications
 
                       <ul>
-                        <li>
-                            ASDF
-                        </li>
 
                         <li>
                             Utility: 3.5/5
@@ -40,6 +37,23 @@ function CourseReviews() {
 
                         <li>
                             Enjoyment: 1.5/5
+                        </li>
+
+                        <li>
+                            This class was... a lot. Carta reviews were pretty cautionary: unclear instruction coupled with esoteric exams and vague 
+                            assignment specs make for an overall negative experience. My experiences were largely similar. I would say 
+                            that I thought the Photo Sharing assignments were where I learned the most. The lectures are usually unrelated
+                            to the current assignment, but they're directly related to exam content (which covered entirely theoretical material
+                            that I generally felt had low practical utility). Professor Rosenblum does post all his past exams, which was 
+                            transparent and useful, so you know what to expect. I took it alone, and looking back I wish I had more friends to collaborate with 
+                            during the later weeks as the assignments became increasingly challenging. The course's open internet / Stack Overflow policy was 
+                            a blessing. 
+
+                            <p>
+                                Utility of 3.5 because the assignments were directly relevant to web development skills, but I felt the lecture/exam content was 
+                                too theoretical to be useful. Probably would take again, but only if I had more friends to lean on.
+                                
+                            </p>
                         </li>
                       </ul>
                       
@@ -219,10 +233,6 @@ function CourseReviews() {
                       CS 106M: Enrichment Adventures in Programming Abstractions
 
                       <ul>
-                        <li>
-                            met some cool people, but the classes were pretty disjoint. the stuff we learned was interesting, but it was more surface level than i expected (though adequate for 1 unit class). one of teh 
-                            dp concepts we covered did help me secure an internship freshman summer, though, so that was nice. 
-                        </li>
 
                         <li>
                             Utility: 1.5/5
@@ -234,6 +244,10 @@ function CourseReviews() {
 
                         <li>
                             Enjoyment: 3/5
+                        </li>
+                        <li>
+                            met some cool people, but the classes were pretty disjoint. the stuff we learned was interesting, but it was more surface level than i expected (though adequate for 1 unit class). one of teh 
+                            dp concepts we covered did help me secure an internship freshman summer, though, so that was nice. 
                         </li>
                       </ul>
                     </li>
