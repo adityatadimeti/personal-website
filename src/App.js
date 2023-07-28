0dd80b3d60bd71813ab07b408b2eb360c2c33dbc
Aditya <adityatadimeti@gmail.com>
2023-07-28 17:38:38 -0400

    changing resume to be url

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { Outlet, Link } from "react-router-dom";
 import React, { useState } from 'react';
-import resume from './resume.pdf';
+import resume from '/Users/adityatadimeti/Desktop/PersonalWebsite/personal-website/src/Aditya_Tadimeti_Resume.pdf';
 import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/entry.webpack";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
+console.log(resume);
 
 function App() {
   const [numPages, setNumPages] = useState(null);
@@ -38,16 +40,10 @@ function App() {
         <p >
           <a href="https://www.linkedin.com/in/adityatadimeti/" target="_blank">LinkedIn</a> {"| "}
           <a href="https://github.com/adityatadimeti/" target="_blank">GitHub</a> {"| "}
-          <a href={resume} target="_blank">Resume</a>
-          {/* <a href="../public/resume.pdf" target="_blank">Resume</a>           */}
-        </p>
+          <a href="https://drive.google.com/file/d/1Mrp_bl65Cjd39E4Bs3AUTgTHadgmpNkV/view?usp=drive_link" target="_blank">Resume</a> 
 
-        {/* <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
-          </Document>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p> */}
+
+        </p>
       </div>
 
       <Outlet />
--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -7,9 +7,6 @@ function Posts() {
       <div class="text">
         <h2>Posts</h2>
 
-        <p>
-          <Link class="textlink" to="/Posts/CourseReviews"> A Review of Stanford Courses (June 6, 2022)</Link>
-        </p>
 
         <p>
           <Link class="textlink" to="/Posts/Writing"> Collection of Writing (July 17, 2022)</Link>
